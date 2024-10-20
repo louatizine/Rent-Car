bbf0724717426ae779440cd8ee476ac4b5c97fcd
zinee <zineeddinelouati.2000@gmail.com>
2024-10-20 12:25:48 +0100

    "Updated frontend code: removed react logo and vite logo, added new components and styles, and refactored main.jsx and App.jsx files."

--- frontend/car-app/src/App.jsx
+++ frontend/car-app/src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home'
+import Search from './Components/Search/Search'
+import Trending from './Components/Trending/Trending'
+import Sellers from './Components/Sellers/Sellers'
+import Auction from './Components/Auction/Auction'
+import Review from './Components/Review/Review'
+import Footer from './Components/Footer/Footer'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='app'>
+      <Navbar />
+      <Home />
+      <Search />
+      <Trending />
+      <Sellers />
+      <Auction />
+      <Review />
+      <Footer />
+    </div>
+
   )
 }
 
